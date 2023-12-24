04c71e4cb4ec13e3134a34fcf1034cae5d018764
Artem <artembolsakov500@gmail.com>
2023-12-25 02:05:35 +0300

    data_3 tests and data_4

--- src/check.py
+++ src/check.py
@@ -14,6 +14,8 @@ with contextlib.redirect_stdout(f):
 @pytest.mark.parametrize('data_in,data_out', [
     ('data1.in', 'data1.out'),
     ('data2.in', 'data2.out'),
+    ('data3.in', 'data3.out'),
+    ('data4_in', 'data4_out'),
 ])
 def test_plus1(data_in, data_out):
     sys.stdin = open(f'../data/{data_in}')
@@ -24,3 +26,18 @@ def test_plus1(data_in, data_out):
     output = f.getvalue().strip()
     expected = open(f'../data/{data_out}').read()
     assert output == expected
+
+
+# @pytest.mark.parametrize('data_in, data_out', [
+#     ('data3.in', 'data3.out'),
+#     ('data4.in', 'data4_out'),
+# ])
+# def test_summ_of_two_numbers(data_in, data_out):
+#     sys.stdin = open(f'../data/{data_in}')
+#
+#     f = io.StringIO()
+#     with contextlib.redirect_stdout(f):
+#         reload(main)
+#     output = f.getvalue().strip()
+#     expected = open(f'../data/{data_out}').read()
+#     assert output == expected
