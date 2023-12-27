cb0f6f05af10a3360bbf80279d1c8e2b85c2ad1c
Artem <artembolsakov500@gmail.com>
2023-12-27 15:09:09 +0300

    Added import_block module

--- src/import_exception.py
+++ src/import_exception.py
@@ -1,15 +1,13 @@
-# import importlib
-#
-#
-# def secure_importer(name, globals=None, locals=None, fromlist=(), level=0):
-#     if name != 'os' and name != 'sys':
-#         print(name, fromlist, level)
-#
-#     frommodule = globals['__name__'] if globals else None
-#     if name == "os" or name == "eval" or name == "exec":
-#         raise ImportError("module '%s' is restricted." % name)
-#
-#     return importlib.__import__(name, globals, locals, fromlist, level)
-#
-#
-# __builtins__.__dict__['__import__'] = secure_importer
+import importlib
+
+
+def secure_importer(name, globals=None, locals=None, fromlist=(), level=0):
+    if name != 'os' and name != 'sys':
+        print(name, fromlist, level)
+
+    if name == "os" or name == "sys":
+        raise ImportError(f"module {name} is restricted.")
+
+    return importlib.__import__(name, globals, locals, fromlist, level)
+
+
--- src/main.py
+++ src/main.py
@@ -1,3 +1,4 @@
+import os
 a = int(input())
 b = int(input())
 print(a + b)
