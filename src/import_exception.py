ac19ebc1c41f2b99df7ba77ab250c218102c8422
Artem <artembolsakov500@gmail.com>
2023-12-27 16:12:37 +0300

    cleaning up code

--- src/import_exception.py
+++ src/import_exception.py
@@ -2,10 +2,9 @@ import importlib
 
 
 def secure_importer(name, globals=None, locals=None, fromlist=(), level=0):
-    #if name != 'os' and name != 'sys':
 
     frommodule = globals['__name__'] if globals else None
-    if name == "os" or name == "sys" and frommodule != "os" and frommodule != "sys" and name == "xyz":
+    if name in ("os", "sys") and frommodule not in ("os", "sys"):
         raise ImportError(f"module {name} is restricted.")
 
     return importlib.__import__(name, globals, locals, fromlist, level)
