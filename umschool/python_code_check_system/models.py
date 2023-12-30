6d2a3335cd8405bc995d20579f0a23d079c05ada
Artem <artembolsakov500@gmail.com>
2023-12-30 12:03:33 +0300

    done models

--- umschool/python_code_check_system/models.py
+++ umschool/python_code_check_system/models.py
@@ -1,3 +1,26 @@
 from django.db import models
 
-# Create your models here.
+
+class Student(models.Model):
+    id = models.BigIntegerField(primary_key=True)
+    solutions = models.ForeignKey('Solution', on_delete=models.SET_NULL, null=True)
+
+
+class Solution(models.Model):
+    id = models.BigIntegerField(primary_key=True)
+    source_code = models.TextField()
+    task_id = models.ForeignKey('Task', on_delete=models.DO_NOTHING)
+    is_accepted = models.BooleanField()
+
+
+class Task(models.Model):
+    id = models.BigIntegerField(primary_key=True)
+    name = models.CharField(max_length=255)
+    description = models.TextField()
+    tests = models.ForeignKey('Tests', on_delete=models.CASCADE)
+
+
+class Tests(models.Model):
+    id = models.BigIntegerField(primary_key=True)
+    input_data = models.TextField()
+    output_data = models.TextField()
