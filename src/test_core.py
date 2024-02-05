b73b0ab2d85e3333c211be388f622ac749045bd7
thehighestmath <kirill.lfybk.rh@gmail.com>
2024-02-05 11:49:01 +0300

    add testcore to github ci, fix some problems

--- src/test_core.py
+++ src/test_core.py
@@ -5,6 +5,6 @@ import pytest
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 
-@pytest.mark.parametrize("filepath, tests, expected_result", [(f"{BASE_DIR}/src/temp_main.py",[(1, 1, 2), (2, 2, 4), (3, 4, 7)], True)])
+@pytest.mark.parametrize("filepath, tests, expected_result", [(f"{BASE_DIR}/src/main.py",[("1", "1", "2"), ("2", "2", "4"), ("3", "4", "7")], True)])
 def test_check(filepath, tests, expected_result):
     assert check(filepath, tests) == expected_result
