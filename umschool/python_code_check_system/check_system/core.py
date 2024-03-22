0af3cdf493de395831dc0d87c73ff15338aa9aef
thehighestmath <kirill.lfybk.rh@gmail.com>
2024-03-22 12:52:40 +0300

    f

--- umschool/python_code_check_system/check_system/core.py
+++ umschool/python_code_check_system/check_system/core.py
@@ -9,16 +9,16 @@ from .types import DataInOut
 
 def check(filepath: str, tests: list[DataInOut]) -> bool:
     code = [
-        """from .utils import secure_importer\n
+        '''from .utils import secure_importer\n
 __builtins__['__import__'] = secure_importer\n
-def main():\n"""
+def main():\n'''
     ]
-    with open(filepath, "r") as file:
+    with open(filepath, 'r') as file:
         lines = file.readlines()
-        code.extend(list(map(lambda line: "    " + line, lines)))
+        code.extend(list(map(lambda line: '    ' + line, lines)))
 
-    with open("python_code_check_system/check_system/temp_main.py", "w") as file:
-        file.write("".join(code))
+    with open('python_code_check_system/check_system/temp_main.py', 'w') as file:
+        file.write(''.join(code))
 
     true_mas = []
     signal.alarm(5)
@@ -29,31 +29,31 @@ def main():\n"""
     for test in tests:
         f = io.StringIO()
         with contextlib.redirect_stdout(f):
-            with tempfile.NamedTemporaryFile(mode="w+", delete=False) as tmpfile:
-                tmpfile.write("\n".join(test.input_data))
+            with tempfile.NamedTemporaryFile(mode='w+', delete=False) as tmpfile:
+                tmpfile.write('\n'.join(test.input_data))
                 tmpfile.flush()
                 tmpfile.seek(0)
                 sys.stdin = tmpfile
             try:
-                with open(filepath, "r") as fp:
+                with open(filepath, 'r') as fp:
                     file_content = fp.read()
-                    if "eval(" in file_content or "exec(" in file_content:
-                        raise FunctionUsageError("Forbidden function call")
+                    if 'eval(' in file_content or 'exec(' in file_content:
+                        raise FunctionUsageError('Forbidden function call')
                 temp_main.main()
             except TimeoutError:
-                sys.stderr.write("\nERROR: function call timed out\n")
+                sys.stderr.write('\nERROR: function call timed out\n')
                 true_mas.append(False)
                 continue
             except MemoryError:
-                sys.stderr.write("\nERROR: Memory Exception\n")
+                sys.stderr.write('\nERROR: Memory Exception\n')
                 true_mas.append(False)
                 continue
             except SyntaxError:
-                sys.stderr.write("\nERROR: SyntaxError\n")
+                sys.stderr.write('\nERROR: SyntaxError\n')
                 true_mas.append(False)
                 continue
             except FunctionUsageError:
-                sys.stderr.write("\nERROR: FunctionUsageError\n")
+                sys.stderr.write('\nERROR: FunctionUsageError\n')
                 true_mas.append(False)
                 continue
             finally:
