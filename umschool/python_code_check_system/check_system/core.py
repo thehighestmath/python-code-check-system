fc801aa27419428ba3b5e802250b19fd958259e8
thehighestmath <kirill.lfybk.rh@gmail.com>
2024-03-22 12:51:50 +0300

    f

--- umschool/python_code_check_system/check_system/core.py
+++ umschool/python_code_check_system/check_system/core.py
@@ -2,6 +2,7 @@ import contextlib
 import io
 import signal
 import sys
+import tempfile
 from .exceptions import FunctionUsageError
 from .types import DataInOut
 
@@ -12,12 +13,12 @@ def check(filepath: str, tests: list[DataInOut]) -> bool:
 __builtins__['__import__'] = secure_importer\n
 def main():\n"""
     ]
-    with open(filepath, 'r') as file:
+    with open(filepath, "r") as file:
         lines = file.readlines()
-        code.extend(list(map(lambda line: '    ' + line, lines)))
+        code.extend(list(map(lambda line: "    " + line, lines)))
 
     with open("python_code_check_system/check_system/temp_main.py", "w") as file:
-        file.write(''.join(code))
+        file.write("".join(code))
 
     true_mas = []
     signal.alarm(5)
@@ -28,13 +29,13 @@ def main():\n"""
     for test in tests:
         f = io.StringIO()
         with contextlib.redirect_stdout(f):
-            with open("../data/data.in", "w") as fp:
-                fp.write("\n".join(test.input_data))
-            with open("../data/data.out", "w") as fp:
-                fp.write(test.output_data)
+            with tempfile.NamedTemporaryFile(mode="w+", delete=False) as tmpfile:
+                tmpfile.write("\n".join(test.input_data))
+                tmpfile.flush()
+                tmpfile.seek(0)
+                sys.stdin = tmpfile
             try:
-                sys.stdin = open("../data/data.in")
-                with open(filepath, 'r') as fp:
+                with open(filepath, "r") as fp:
                     file_content = fp.read()
                     if "eval(" in file_content or "exec(" in file_content:
                         raise FunctionUsageError("Forbidden function call")
@@ -44,7 +45,7 @@ def main():\n"""
                 true_mas.append(False)
                 continue
             except MemoryError:
-                sys.stderr.write('\nERROR: Memory Exception\n')
+                sys.stderr.write("\nERROR: Memory Exception\n")
                 true_mas.append(False)
                 continue
             except SyntaxError:
@@ -57,9 +58,7 @@ def main():\n"""
                 continue
             finally:
                 signal.alarm(5)
-            output = f.getvalue().strip()
-            expected = open(f'../data/data.out').read().strip()
-            with open(f'../data/data_test', 'w') as fp:
-                fp.write(str(expected))
-            true_mas.append(expected == output)
+            actual = f.getvalue().strip()
+            expected = test.output_data
+            true_mas.append(expected == actual)
     return all(true_mas)
