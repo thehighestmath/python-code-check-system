866985595b1956c20d123c0f866b7cfddd8cc497
Egor <59351926+GarryKoker@users.noreply.github.com>
2024-03-27 05:10:35 +0800

    Class test (#28)
    Co-authored-by: thehighestmath <kirill.lfybk.rh@gmail.com>

---
+++ umschool/python_code_check_system/check_system/class_test/test_class.py
@@ -0,0 +1,19 @@
+import pytest
+
+from python_code_check_system.check_system.types import DataArgs
+
+from python_code_check_system.check_system.class_test.student_code import SumClass
+
+
+@pytest.mark.parametrize(
+    "data_args",
+    [
+        DataArgs(input_args=[3, 5], output_data=8),
+        DataArgs(input_args=[4], input_kwargs={"b": 1}, output_data=5),
+        DataArgs(input_kwargs={"a": 1, "b": 2}, output_data=3),
+    ],
+)
+def test_sum_class(data_args: DataArgs):
+    obj = SumClass(*data_args.input_args, **data_args.input_kwargs)
+
+    assert obj.sum() == data_args.output_data
--- umschool/python_code_check_system/check_system/types.py
+++ umschool/python_code_check_system/check_system/types.py
@@ -1,7 +1,16 @@
-from dataclasses import dataclass
+from dataclasses import dataclass, field
 
 
+# used for stdin/stdout check
 @dataclass(kw_only=True, frozen=True)
 class DataInOut:
     input_data: list[str]
     output_data: str  # TODO: it can be list[str]
+
+
+# used for class test
+@dataclass(kw_only=True, frozen=True)
+class DataArgs:
+    input_args: list = field(default_factory=list)
+    input_kwargs: dict = field(default_factory=dict)
+    output_data: ...
