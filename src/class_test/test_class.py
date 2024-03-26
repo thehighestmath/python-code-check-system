142d9bb121d7633b8b0c6d479eaaa08d57475b33
thehighestmath <kirill.lfybk.rh@gmail.com>
2024-03-26 23:33:20 +0300

    f

--- src/class_test/test_class.py
+++ src/class_test/test_class.py
@@ -1,7 +1,7 @@
 import pytest
 from student_code import Operations
 
-with open("Test_values.txt") as file:
+with open("test_values.txt") as file:
     list_tests_values = file.readlines()
 
 length = len(list_tests_values)
@@ -9,21 +9,16 @@ length = len(list_tests_values)
 tuples = []
 
 for row in list_tests_values:
-
-    test_row = row.replace("\n","").split()
-
+    test_row = row.replace("\n", "").split()
     test_row = list(map(int, test_row))
-
     tuple1 = tuple(test_row)
-
     tuples.append(tuple1)
+
 
 @pytest.mark.parametrize("a, b, expected_result", tuples)
 def test_actions(a, b, expected_result):
-    obj = Operations(a,b)
+    obj = Operations(a, b)
 
     assert obj.a == a
-
     assert obj.b == b
-
     assert obj.sum() == expected_result
