f9b106faa256b7672321c7b836c950a8bd66194e
thehighestmath <kirill.lfybk.rh@gmail.com>
2024-03-27 10:47:12 +0300

    f

--- umschool/python_code_check_system/check_system/core.py
+++ umschool/python_code_check_system/check_system/core.py
@@ -11,28 +11,26 @@ from python_code_check_system.check_system.exceptions import FunctionUsageError
 from python_code_check_system.check_system.types import DataInOut
 
 
-
-
 def check(filepath: str, tests: list[DataInOut]) -> bool:
-    uid = str(uuid.uuid4()).replace('-', '')
+    uid = str(uuid.uuid4()).replace("-", "")
     file_name = f"{uid}.py"
     code = [
-        '''from python_code_check_system.check_system.utils import secure_importer\n
+        """from python_code_check_system.check_system.utils import secure_importer\n
 __builtins__['__import__'] = secure_importer\n
-def main():\n'''
+def main():\n"""
     ]
-    with open(filepath, 'r') as file:
+    with open(filepath, "r") as file:
         lines = file.readlines()
-        code.extend(list(map(lambda line: '    ' + line, lines)))
+        code.extend(list(map(lambda line: "    " + line, lines)))
 
-    with open(f'python_code_check_system/check_system/{file_name}', 'w') as file:
-        file.write(''.join(code))
+    with open(f"python_code_check_system/check_system/{file_name}", "w") as file:
+        file.write("".join(code))
 
     true_mas = []
     signal.alarm(5)
     # memory_limit(5)
 
-    module_path = f'python_code_check_system.check_system.{uid}'
+    module_path = f"python_code_check_system.check_system.{uid}"
     temp_main = importlib.import_module(module_path)
 
     for test in tests:
@@ -40,28 +38,28 @@ def main():\n'''
         with contextlib.redirect_stdout(f):
             Path("../data/").mkdir(exist_ok=True)
             with open("../data/data.in", "w") as fp:
-                fp.write('\n'.join(test.input_data))
+                fp.write("\n".join(test.input_data))
             try:
                 sys.stdin = open("../data/data.in")
-                with open(filepath, 'r') as fp:
+                with open(filepath, "r") as fp:
                     file_content = fp.read()
-                    if 'eval(' in file_content or 'exec(' in file_content:
-                        raise FunctionUsageError('Forbidden function call')
+                    if "eval(" in file_content or "exec(" in file_content:
+                        raise FunctionUsageError("Forbidden function call")
                 temp_main.main()
             except TimeoutError:
-                sys.stderr.write('\nERROR: function call timed out\n')
+                sys.stderr.write("\nERROR: function call timed out\n")
                 true_mas.append(False)
                 continue
             except MemoryError:
-                sys.stderr.write('\nERROR: Memory Exception\n')
+                sys.stderr.write("\nERROR: Memory Exception\n")
                 true_mas.append(False)
                 continue
             except SyntaxError:
-                sys.stderr.write('\nERROR: SyntaxError\n')
+                sys.stderr.write("\nERROR: SyntaxError\n")
                 true_mas.append(False)
                 continue
             except FunctionUsageError:
-                sys.stderr.write('\nERROR: FunctionUsageError\n')
+                sys.stderr.write("\nERROR: FunctionUsageError\n")
                 true_mas.append(False)
                 continue
             finally:
@@ -70,6 +68,6 @@ def main():\n'''
             expected = test.output_data
             # raise Exception (str(f'{actual}, {expected}'))
             true_mas.append(expected == actual)
-    os.remove(f'python_code_check_system/check_system/{file_name}')
+    os.remove(f"python_code_check_system/check_system/{file_name}")
     print(true_mas)
     return all(true_mas)
