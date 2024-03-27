8ace8896b59ae68efbf96229e8bf6cce3df0df1c
Kirill Kryzhanovsky <42893255+thehighestmath@users.noreply.github.com>
2024-03-27 11:14:18 +0300

    multi src codes for testing  (#33)

--- umschool/python_code_check_system/check_system/core.py
+++ umschool/python_code_check_system/check_system/core.py
@@ -2,14 +2,20 @@ import contextlib
 import io
 import signal
 import sys
+import os
 from pathlib import Path
-from .exceptions import FunctionUsageError
-from .types import DataInOut
+import importlib
+import uuid
+
+from python_code_check_system.check_system.exceptions import FunctionUsageError
+from python_code_check_system.check_system.types import DataInOut
 
 
 def check(filepath: str, tests: list[DataInOut]) -> bool:
+    uid = 'module_' + str(uuid.uuid4()).replace('-', '')
+    file_name = f'{uid}.py'
     code = [
-        '''from .utils import secure_importer\n
+        '''from python_code_check_system.check_system.utils import secure_importer\n
 __builtins__['__import__'] = secure_importer\n
 def main():\n'''
     ]
@@ -17,23 +23,24 @@ def main():\n'''
         lines = file.readlines()
         code.extend(list(map(lambda line: '    ' + line, lines)))
 
-    with open('python_code_check_system/check_system/temp_main.py', 'w') as file:
+    with open(f'python_code_check_system/check_system/{file_name}', 'w') as file:
         file.write(''.join(code))
 
     true_mas = []
     signal.alarm(5)
     # memory_limit(5)
-    f = io.StringIO()
-    with contextlib.redirect_stdout(f):
-        from python_code_check_system.check_system import temp_main
+
+    module_path = f'python_code_check_system.check_system.{uid}'
+    temp_main = importlib.import_module(module_path)
+
     for test in tests:
         f = io.StringIO()
         with contextlib.redirect_stdout(f):
-            Path("../data/").mkdir(exist_ok=True)
-            with open("../data/data.in", "w") as fp:
+            Path('../data/').mkdir(exist_ok=True)
+            with open('../data/data.in', 'w') as fp:
                 fp.write('\n'.join(test.input_data))
             try:
-                sys.stdin = open("../data/data.in")
+                sys.stdin = open('../data/data.in')
                 with open(filepath, 'r') as fp:
                     file_content = fp.read()
                     if 'eval(' in file_content or 'exec(' in file_content:
@@ -60,4 +67,5 @@ def main():\n'''
             actual = f.getvalue().strip()
             expected = test.output_data
             true_mas.append(expected == actual)
+    os.remove(f'python_code_check_system/check_system/{file_name}')
     return all(true_mas)
--- umschool/python_code_check_system/check_system/tests/test_stdin_stdout.py
+++ umschool/python_code_check_system/check_system/tests/test_stdin_stdout.py
@@ -6,12 +6,11 @@ from python_code_check_system.check_system.types import DataInOut
 
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-
 @pytest.mark.parametrize(
     "filepath, tests",
     [
         (
-            f"{BASE_DIR}/check_system/main.py",
+            f"{BASE_DIR}/tests/sample_src/main_plus.py",
             [
                 DataInOut(input_data=["1", "1"], output_data="2"),
                 DataInOut(input_data=["2", "2"], output_data="4"),
@@ -20,5 +19,22 @@ BASE_DIR = Path(__file__).resolve().parent.parent
         )
     ],
 )
-def test_check(filepath, tests):
+def test_check_plus(filepath, tests):
+    assert check(filepath, tests)
+
+
+@pytest.mark.parametrize(
+    "filepath, tests",
+    [
+        (
+            f"{BASE_DIR}/tests/sample_src/main_sub.py",
+            [
+                DataInOut(input_data=["1", "1"], output_data="0"),
+                DataInOut(input_data=["-2", "-5"], output_data="3"),
+                DataInOut(input_data=["3", "4"], output_data="-1"),
+            ],
+        )
+    ],
+)
+def test_check_minus(filepath, tests):
     assert check(filepath, tests)
