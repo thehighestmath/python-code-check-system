bdfe2505b242bbe8d1c8304ac538721008fcb0ed
thehighestmath <kirill.lfybk.rh@gmail.com>
2024-04-20 13:59:11 +0300

    f

--- umschool/entrypoint.sh
+++ umschool/entrypoint.sh
@@ -9,7 +9,7 @@ then
     echo "Creating superuser"
     python manage.py shell -c "from django.contrib.auth import get_user_model; User = get_user_model(); User.objects.create_superuser('$DJANGO_SUPERUSER_USERNAME', '$DJANGO_SUPERUSER_EMAIL', '$DJANGO_SUPERUSER_PASSWORD')"
 else
-    echo "Existiing superuser with username $DJANGO_SUPERUSER_USERNAME"
+    echo "Superuser with username $DJANGO_SUPERUSER_USERNAME exists"
 fi
 
 python manage.py runserver 0.0.0.0:8000
