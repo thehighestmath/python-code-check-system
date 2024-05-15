533e9f4ace21c3b3243cebefedd7b1eac15fac9c
thehighestmath <kirill.lfybk.rh@gmail.com>
2024-05-15 23:00:08 +0300

    add eval,exec check

--- umschool/python_code_check_system/check_system/tests/test_stdin_stdout.py
+++ umschool/python_code_check_system/check_system/tests/test_stdin_stdout.py
@@ -6,83 +6,85 @@ from python_code_check_system.check_system.types import DataInOut
 
 BASE_DIR = Path(__file__).resolve().parent.parent
 
+
 @pytest.mark.parametrize(
     "filepath, tests",
     [
-        (
+        pytest.param(
             f"{BASE_DIR}/tests/sample_src/main_plus.py",
             [
                 DataInOut(input_data=["1", "1"], output_data=["2"]),
                 DataInOut(input_data=["2", "2"], output_data=["4"]),
                 DataInOut(input_data=["3", "4"], output_data=["7"]),
             ],
-        )
-    ],
-)
-def test_check_plus(filepath, tests):
-    assert check(filepath, tests).verdict
-
-
-@pytest.mark.parametrize(
-    "filepath, tests",
-    [
-        (
+            id="plus",
+        ),
+        pytest.param(
             f"{BASE_DIR}/tests/sample_src/main_sub.py",
             [
                 DataInOut(input_data=["1", "1"], output_data=["0"]),
                 DataInOut(input_data=["-2", "-5"], output_data=["3"]),
                 DataInOut(input_data=["3", "4"], output_data=["-1"]),
             ],
-        )
+            id="sub",
+        ),
     ],
 )
-def test_check_minus(filepath, tests):
+def test_check_simple_math(filepath, tests):
     assert check(filepath, tests).verdict
 
 
 @pytest.mark.parametrize(
-    "filepath, tests",
+    "filepath, tests, expected_error",
     [
-        (
+        pytest.param(
             f"{BASE_DIR}/tests/sample_src/main_syntax_error.py",
             [
                 DataInOut(input_data=["1", "1"], output_data=["0"]),
             ],
-        )
-    ],
-)
-def test_check_syntax_error(filepath, tests):
-    r = check(filepath, tests)
-    print(r.error_verbose)
-    assert check(filepath, tests).error_verbose == 'SyntaxError'
-
-
-@pytest.mark.timeout(5)
-@pytest.mark.parametrize(
-    "filepath, tests",
-    [
-        (
-            f"{BASE_DIR}/tests/sample_src/main_loop.py",
-            [
-                DataInOut(input_data=[], output_data=[]),
-            ],
-        )
-    ],
-)
-def test_infinity_loop(filepath, tests):
-    assert check(filepath, tests).error_verbose == 'TimeoutError'
-
-
-@pytest.mark.parametrize(
-    "filepath, tests",
-    [
-        (
+            "SyntaxError",
+            id="syntax-error",
+        ),
+        pytest.param(
             f"{BASE_DIR}/tests/sample_src/main_memory.py",
             [
                 DataInOut(input_data=[], output_data=[]),
             ],
-        )
+            "MemoryError",
+            id="memory-out-error",
+        ),
+        pytest.param(
+            f"{BASE_DIR}/tests/sample_src/main_loop.py",
+            [
+                DataInOut(input_data=[], output_data=[]),
+            ],
+            "TimeoutError",
+            id="timeout-error"
+        ),
     ],
 )
-def test_memory_out(filepath, tests):
-    assert check(filepath, tests).error_verbose == 'MemoryError'
+def test_check_errors(filepath, tests, expected_error):
+    assert check(filepath, tests).error_verbose == expected_error
+
+
+@pytest.mark.parametrize(
+    "filepath, tests",
+    [
+        pytest.param(
+            f"{BASE_DIR}/tests/sample_src/main_eval.py",
+            [
+                DataInOut(input_data=["1+1"], output_data=["2"]),
+            ],
+            id="eval-call",
+        ),
+        pytest.param(
+            f"{BASE_DIR}/tests/sample_src/main_exec.py",
+            [
+                DataInOut(input_data=[], output_data=[]),
+            ],
+            id="exec-call",
+        ),
+    ],
+)
+def test_forbidden_function_call(filepath, tests):
+    assert check(filepath, tests).error_verbose == "ForbiddenFunctionCall"
